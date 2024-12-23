8deb102be79737a703c9972bea59c455211606c9
Claas Wenk <124201429+claaswenk@users.noreply.github.com>
2024-12-23 10:42:27 +0100

    Grafische Optimierung main.c
    + kleine Hotfixes an der Einheitlichkeit der Konsolen-Ausgaben 
    + oberste Spalte in csv heißt jz Matrikelnummer statt Studentennummer 
    + ein Bug behoben bei Eingabe von ungültigem Wert in <2>

--- main.c
+++ main.c
@@ -64,7 +64,7 @@ int inputStudent(struct student* sNew) {
 
     // Vorname
     do {
-        printf("\nGeben Sie 'q' ein, um abzubrechen)\nWie lautet der Vorname?: ");
+        printf("\n(Zum Abbrechen: q)\nWie lautet der Vorname?: ");
         int c; //Buffer clearen
         while ((c = getchar()) != '\n' && c != EOF);
 
@@ -76,13 +76,13 @@ int inputStudent(struct student* sNew) {
         }
         checkString = checkInputString(sNew->first_name);
         if (checkString) {
-            printf("\n***Eingabe ungueltig: Nur Buchstaben erlaubt!***\n");
+            printf("\n***Eingabe ungueltig: Es sind nur Buchstaben und keine Sonderzeichen erlaubt***\n");
         }
     } while (checkString);  
 
     // Nachname
     do {
-        printf("\nGeben Sie 'q' ein, um abzubrechen)\nWie lautet der Nachname?: ");
+        printf("\n(Zum Abbrechen: q)\nWie lautet der Nachname?: ");
         fgets(sNew->last_name, sizeof(sNew->last_name), stdin);
         sNew->last_name[0] = toupper(sNew->last_name[0]);
         sNew->last_name[strcspn(sNew->last_name, "\n")] = '\0';
@@ -91,13 +91,13 @@ int inputStudent(struct student* sNew) {
         }
         checkString = checkInputString(sNew->last_name);
         if (checkString) {
-            printf("\n***Eingabe ungueltig: Nur Buchstaben erlaubt!***\n");
+            printf("\n***Eingabe ungueltig: Es sind nur Buchstaben und keine Sonderzeichen erlaubt***\n");
         }
     } while (checkString);  
 
     // Studiengang
     do {
-        printf("\nGeben Sie 'q' ein, um abzubrechen)\nWelches Studienfach wird belegt?: ");
+        printf("\n(Zum Abbrechen: q)\nWelches Studienfach wird belegt?: ");
         scanf("%99s", sNew->course);
         sNew->course[0] = toupper(sNew->course[0]);
         if (strcmp(sNew->course, "q") == 0) {
@@ -105,48 +105,48 @@ int inputStudent(struct student* sNew) {
         }
         checkString = checkInputString(sNew->course);
         if (checkString) {
-            printf("\n***Eingabe ungueltig: Nur Buchstaben erlaubt!***\n");
+            printf("\n***Eingabe ungueltig: Es sind nur Buchstaben und keine Sonderzeichen erlaubt***\n");
         }
     } while (checkString);  
 
     // Geburtsdatum
     do {
-        printf("\nGeben Sie 'q' ein, um abzubrechen)\nWie lautet das Geburtsdatum? (TT.MM.JJJJ): ");
+        printf("\n(Zum Abbrechen: q)\nWie lautet das Geburtsdatum? (TT.MM.JJJJ): ");
         scanf("%2s.%2s.%4s", sNew->birthday.day, sNew->birthday.month, sNew->birthday.year);
         if (strcmp(sNew->birthday.day, "q") == 0) {
             return 0;  //Hab abbruch mittendrin eingebaut
         }
         checkDate = checkInputDate(sNew->birthday.day, sNew->birthday.month, sNew->birthday.year, whichDate, NULL, NULL, NULL);
         if (checkDate == 0) {
-            printf("\n***Geburtsdatum ist ungültig!******\n");
+            printf("\n***Geburtsdatum ist ungueltig***\n");
         }
     } while (checkDate == 0); 
 
     // Startdatum
     whichDate = 2;
     do {
-        printf("\nGeben Sie 'q' ein, um abzubrechen)\nGeben Sie das Startdatum ein (TT.MM.JJJJ): ");
+        printf("\n(Zum Abbrechen: q)\nGeben Sie das Startdatum ein (TT.MM.JJJJ): ");
         scanf("%2s.%2s.%4s", sNew->start.day, sNew->start.month, sNew->start.year);
         if (strcmp(sNew->start.day, "q") == 0) {
             return 0;  //Hab abbruch mittendrin eingebaut
         }
         checkDate = checkInputDate(sNew->start.day, sNew->start.month, sNew->start.year, whichDate, sNew->birthday.day, sNew->birthday.month, sNew->birthday.year);
         if (checkDate == 0) {
-            printf("\n***Startdatum ist ungültig!******\n");
+            printf("\n***Startdatum ist ungueltig***\n");
         }
     } while (checkDate == 0);  
 
     // Enddatum
     whichDate = 1;
     do {
-        printf("\nGeben Sie 'q' ein, um abzubrechen)\nGeben Sie das Enddatum ein (TT.MM.JJJJ): ");
+        printf("\n(Zum Abbrechen: q)\nGeben Sie das Enddatum ein (TT.MM.JJJJ): ");
         scanf("%2s.%2s.%4s", sNew->end.day, sNew->end.month, sNew->end.year);
         if (strcmp(sNew->end.day, "q") == 0) {
             return 0;  //Hab abbruch mittendrin eingebaut
         }
         checkDate = checkInputDate(sNew->end.day, sNew->end.month, sNew->end.year, whichDate, sNew->start.day, sNew->start.month, sNew->start.year);
         if (checkDate == 0) {
-            printf("\n***Enddatum ist ungültig!***\n");
+            printf("\n***Enddatum ist ungueltig***\n");
         }
     } while (checkDate == 0); 
 
@@ -198,12 +198,12 @@ void deleteStudent(int student_number, struct student **s1) {
                 }
             }
             free(current);
-            printf("\n***Student erfolgreich gelöscht***\n");
+            printf("\n***Studenten erfolgreich geloescht***\n");
             return;
         }
         current = current->next;
     }
-    printf("\n***Studentennummer nicht gefunden***\n");
+    printf("\n***Matrikelnummer nicht gefunden***\n");
 }
 
 
@@ -220,7 +220,7 @@ void addStudent(struct student** s1) {
     }
 
     if (inputStudent(sNew) == 0) {
-        printf("Eingabe wurde abgebrochen, daher wird kein Student hinzugefügt.\n");
+        printf("\n***Eingabe wurde vom Benutzer abgebrochen, daher wurde kein Student hinzugefuegt***\n");
         free(sNew);  // Input abgebrochen
         return; 
     }
@@ -264,7 +264,7 @@ int max = findMaxStudentNumber(*s1);
 if (sNew->student_number == 99999999)
 {
     free(sNew);
-    printf("Das System ist voll, neuer Student konnte nicht gespeichert werden");
+    printf("***Das System ist voll, neuer Student konnte nicht gespeichert werden***");
     return;
 }else sNew->student_number = max + 1;
 
@@ -287,7 +287,7 @@ void printAllStudents(struct student *s1) {
         printf("\nGeburtsdatum:\t%s.%s.%s", s1->birthday.day, s1->birthday.month, s1->birthday.year);
         printf("\nStartdatum:\t%s.%s.%s", s1->start.day, s1->start.month, s1->start.year);
         printf("\nEnddatum:\t%s.%s.%s", s1->end.day, s1->end.month, s1->end.year);
-        printf("\nMatrikelnummmer:%d",s1->student_number);
+        printf("\nMatrikelnr.: %d",s1->student_number);
         if(s1->next == NULL)                    /* Leerzeile wird nach letztem Student geprintet*/
         {
             printf("\n");
@@ -312,7 +312,7 @@ void printSingleStudentInformation(struct student *s1){
     printf("\nGeburtsdatum:\t%s.%s.%s", s1->birthday.day, s1->birthday.month, s1->birthday.year);
     printf("\nStartdatum:\t%s.%s.%s", s1->start.day, s1->start.month, s1->start.year);
     printf("\nEnddatum:\t%s.%s.%s", s1->end.day, s1->end.month, s1->end.year);
-    printf("\nMatrikelnummmer:%d",s1->student_number);
+    printf("\nMatrikelnr.: %d",s1->student_number);
 }
 
 void printStudent(int student_number, struct student* s1)
@@ -320,6 +320,12 @@ void printStudent(int student_number, struct student* s1)
     int found = 0;
     struct student* current = s1;
 
+    // Fall: Die Liste ist leer
+    if (current == NULL) {
+        printf("\n***Keine Studenten in der Liste***\n");
+        return;
+    }
+    
     if(current->student_number == student_number) {
         printSingleStudentInformation(s1);
         found = 1;
@@ -332,7 +338,7 @@ void printStudent(int student_number, struct student* s1)
         };
         current = current->next;
     }
-    if(found == 0) printf("\n***Matrikelnummer nicht gefunden***");
+    if(found == 0) printf("\n***Matrikelnummer nicht gefunden***\n");
 }
 
 void interfaceSwitch(int choice, struct student **s1) {
@@ -344,7 +350,7 @@ void interfaceSwitch(int choice, struct student **s1) {
         break;
     case '2':
         printf("------------------- <2> Studenten anzeigen <4> -------------------");
-        printf("\nWelchen Studenten möchten Sie angezeigt bekommen (Matrikelnummer): ");
+        printf("\nWelchen Studenten moechten Sie angezeigt bekommen? (Matrikelnummer): ");
         scanf("%d", &student_number);
         printStudent(student_number, *s1);
         break;
@@ -353,7 +359,7 @@ void interfaceSwitch(int choice, struct student **s1) {
         break;
     case '4':
         printf("------------------- <4> Studenten loeschen <4> -------------------");
-        printf("\nWelchen Studenten moechten Sie loeschen (Matrikelnummer): ");
+        printf("\nWelchen Studenten moechten Sie loeschen? (Matrikelnummer): ");
         scanf("%d", &student_number);
         deleteStudent(student_number, s1);
         break;
@@ -398,7 +404,7 @@ void save(struct student *s1) {
     }
     
 
-    fprintf(file, "Studentennummer,Vorname,Nachname,Studienfach,Geburtsdatum,Startdatum,Enddatum\n");
+    fprintf(file, "Matrikelnummer,Vorname,Nachname,Studienfach,Geburtsdatum,Startdatum,Enddatum\n");
     
 
     while (s1 != NULL) {
